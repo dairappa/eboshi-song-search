34baceb57537aa958c81badc4fdfe65cc714b975
dairappa <dairappa@gmail.com>
2021-02-19 11:31:27 +0900

    fontをjsから設定するようにし、環境変数からbasepathを取得する

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
-const { PHASE_PRODUCTION_BUILD, PHASE_EXPORT } = require('next/constants');
+const { PHASE_PRODUCTION_BUILD, PHASE_EXPORT } = require('next/constants')
 
 module.exports = (phase) => {
   if (phase === PHASE_PRODUCTION_BUILD || phase === PHASE_EXPORT) {
     return {
       basePath: '/liver-song-search',
-      assetPrefix: '/liver-song-search/'
-    }; //for github pages
+      assetPrefix: '/liver-song-search/',
+      env: {
+        basePath: '/liver-song-search',
+      },
+    } //for github pages
   }
-  return {};
-};
+  return {}
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,19 @@ import React from 'react'
 import 'nes.css/css/nes.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const basePath = process.env.basePath ?? ''
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <style global jsx>{`
+        @font-face {
+          font-family: 'misakigothic';
+          src: url('${basePath}/fonts/misaki_gothic.ttf') format('truetype');
+        }
+      `}</style>
+    </>
+  )
 }
 
 export default MyApp
