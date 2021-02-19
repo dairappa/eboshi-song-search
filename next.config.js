3769c71ce1ab6f7d2e9702e1f4518ae890704d0a
dairappa <dairappa@gmail.com>
2021-02-19 09:23:46 +0900

    next-configのエラーを修正、警告が出ていたAPIディレクトリを削除

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-import { PHASE_PRODUCTION_BUILD } from 'next/constants'
+const { PHASE_PRODUCTION_BUILD } = require('next/constants');
 
 module.exports = (phase) => {
   if (phase === PHASE_PRODUCTION_BUILD) {
-    return { assetPrefix: 'liver-song-search' } //for github pages build
+    return { assetPrefix: 'liver-song-search' }; //for github pages
   }
-  return {}
-}
+  return {};
+};
