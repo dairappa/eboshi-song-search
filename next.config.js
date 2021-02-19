66b62bea99baa6fa547ca60aa757dd28cf18f737
dairappa <dairappa@gmail.com>
2021-02-19 09:49:27 +0900

    basepath機能修正

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { PHASE_PRODUCTION_BUILD, PHASE_EXPORT } = require('next/constants');
 
 module.exports = (phase) => {
   if (phase === PHASE_PRODUCTION_BUILD || phase === PHASE_EXPORT) {
-    return { basePath: 'liver-song-search' }; //for github pages
+    return { basePath: '/liver-song-search' }; //for github pages
   }
   return {};
 };
