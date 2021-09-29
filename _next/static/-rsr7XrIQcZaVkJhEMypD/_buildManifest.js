04f5bac545c216445f1904e2b70ba42bb03ffc5c
dairappa <53238927+dairappa@users.noreply.github.com>
2021-09-29 13:10:29 +0000

    Deploying to github-pages from  @ ff6158600088ce194b9360e2f2e98d0c969474ad 🚀

--- _next/static/-rsr7XrIQcZaVkJhEMypD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-6b237f2bbd40eb177540.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a548b957b00a03aef364.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
