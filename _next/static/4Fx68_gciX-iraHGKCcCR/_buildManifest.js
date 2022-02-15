cae2afe20755a5380600ecdb348e26116692ce56
dairappa <53238927+dairappa@users.noreply.github.com>
2022-02-15 21:35:13 +0000

    Deploying to github-pages from  @ d9f5babb6ce3dd6de53c59579d80529eefe8610e 🚀

---
+++ _next/static/4Fx68_gciX-iraHGKCcCR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ea6e2e118dda450e45e0.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
